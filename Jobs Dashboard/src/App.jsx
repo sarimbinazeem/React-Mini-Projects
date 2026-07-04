0194165875261709a5a57cbcfcc93404018a4170
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-04 10:39:02 +0500

    Added Folders and Created Components file

---
+++ Jobs Dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cards from './components/cards'
+
+const App = () => {
+  return (
+    <div className='jobsContainer'>
+      <Cards/>
+    </div>
+  )
+}
+
+export default App
