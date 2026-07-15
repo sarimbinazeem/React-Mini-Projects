0b8955d415726a61cf27ba995ca41393887f795a
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-15 21:28:03 +0500

    Project 2: Completed FAQ item file

--- FAQ/src/components/FAQitem.jsx
+++ FAQ/src/components/FAQitem.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const FAQitem = () => {
+const FAQitem = ({question,answer}) => {
   return (
     <div>
+      <div>
+          <h2>Frequently Asked Questions</h2>
+          <p>Everything you need to know.</p>        
+      </div>
+
+      <div>
+        <details>
+          <summary>
+              {question}
+          </summary>
+
+          <p> {answer}</p>
+        </details>
+      </div>
       
     </div>
   )
