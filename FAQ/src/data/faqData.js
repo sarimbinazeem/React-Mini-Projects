c3d8f8f4d67c5ce0c8830636339a2b8c60ef333e
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-15 20:52:30 +0500

    Project 2: Added data

---
+++ FAQ/src/data/faqData.js
@@ -0,0 +1,40 @@
+const faqData = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer:
+      "React is a JavaScript library used to build fast and interactive user interfaces using reusable components.",
+  },
+  {
+    id: 2,
+    question: "Why should I learn React?",
+    answer:
+      "React is one of the most popular frontend libraries and is widely used in modern web development.",
+  },
+  {
+    id: 3,
+    question: "What is JSX?",
+    answer:
+      "JSX is a syntax extension that lets you write HTML-like code inside JavaScript, making UI development easier.",
+  },
+  {
+    id: 4,
+    question: "What is useState?",
+    answer:
+      "useState is a React Hook that allows functional components to store and update data called state.",
+  },
+  {
+    id: 5,
+    question: "What are Props?",
+    answer:
+      "Props are read-only values passed from a parent component to a child component to share data.",
+  },
+  {
+    id: 6,
+    question: "Is React enough for frontend development?",
+    answer:
+      "React is a powerful library, but you'll also need HTML, CSS, JavaScript, routing, state management, and API handling to build complete frontend applications.",
+  },
+];
+
+export default faqData;
